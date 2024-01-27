4a57594ac9b3244bcf58d68d4e7ed453191517fb
Stefan Olsen <stol@cloudfactory.dk>
2024-01-27 09:20:26 +0100

    more nvmi

--- .config/nvim/lua/stefanolsenn/packer.lua
+++ .config/nvim/lua/stefanolsenn/packer.lua
@@ -12,13 +12,7 @@ return require('packer').startup(function(use)
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
-  })
+  use 'navarasu/onedark.nvim'
 
   use({
       "folke/trouble.nvim",
