e77fc9e3cc65be8bb33f4b67f2c83c3d0b9d4124
Stefan <23049696+stefanolsenn@users.noreply.github.com>
2024-05-11 20:31:28 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,12 +10,19 @@ git clone --bare git@github.com:stefanolsenn/dotfiles.git $HOME/.dotfiles
 mkdir -p $HOME/.dotfiles-backups
 dotfiles checkout -q &> /dev/null
 
-if [ $? = 0 ]; then
+if [[ $? == 0 ]]; then
   echo "Checked out dotfiles.";
+else
+  echo "Backing up pre-existing dot files.";
+  dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $HOME/.dotfiles-backups/{}
+  
+  if [[ $? == 0]]; then
+     echo "Something went wronng"
   else
-    echo "Backing up pre-existing dot files.";
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $HOME/.dotfiles-backups/{}
-fi;
+     dotfiles checkout
+     dotfiles config status.showUntrackedFiles no
+     echo "OK!"
+  fi
+fi
 
-dotfiles checkout
-dotfiles config status.showUntrackedFiles no
+
