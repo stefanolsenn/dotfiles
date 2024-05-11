97ff8ddce342f9c57567bbd0c7541a88e94d6f8e
Stefan <23049696+stefanolsenn@users.noreply.github.com>
2024-05-11 20:26:15 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+function dotfiles {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+rm -rf $HOME/.dotfiles
+git clone --bare git@github.com:stefanolsenn/dotfiles.git $HOME/.dotfiles
+
+mkdir -p $HOME/.dotfiles-backups
+dotfiles checkout -q &> /dev/null
+
+if [ $? = 0 ]; then
+  echo "Checked out dotfiles.";
+  else
+    echo "Backing up pre-existing dot files.";
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $HOME/.dotfiles-backups/{}
+fi;
+
+dotfiles checkout
+dotfiles config status.showUntrackedFiles no
