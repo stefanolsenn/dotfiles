c9d2ede1c68792522ab5d69dfcdfedc5cc13b9ec
Stefan Olsen <stol@cloudfactory.dk>
2024-05-14 20:14:25 +0200

    changed setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,31 @@
 #!/bin/bash -e
 
-if [ -d "$HOME/.dotfiles" ]; then
-    rm -rf "$HOME/.dotfiles"
-fi
+current_dir=$(pwd)
+dotfiles_dir="$HOME/.dotfiles"
+dotfiles_dir_backups="$HOME/.dotfiles-backups"
 
+function dotfiles {
+   /usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" "$@"
+}
+function remove_tracked_files() {
+	if [[ -d $dotfiles_dir ]]; then
+		cd $dotfiles_dir
+		tracked_files=$(git ls-files)
+
+		for file in $tracked_files;
+		do
+			rm -f $file
+		done
+
+		rm -rf $dotfiles_dir
+		rm -rf $dotfiles_dir_backups
+		cd $current_dir
+	fi
+}
+
+remove_tracked_files
 git clone --bare git@github.com:stefanolsenn/dotfiles.git "$HOME/.dotfiles"
-mkdir -p "$HOME/.dotfiles-backups"
+mkdir -p $dotfiles_dir_backups
 
 if dotfiles checkout -q &>/dev/null; then
     echo "Checked out dotfiles."
@@ -13,7 +33,7 @@ if dotfiles checkout -q &>/dev/null; then
 else
     echo "Backing up pre-existing dot files."
 
-    dotfiles checkout 2>&1 | grep "^\s+" | awk '{print $1}' | xargs -I{} mv {} "$HOME/.dotfiles-backups/{}"
+    dotfiles checkout 2>&1 | grep "^\s+" | awk '{print $1}' | xargs -I{} mv {} "$dotfiles_dir_backups/{}"
 
     if [ $? -ne 0 ]; then
         echo "Failed to backup dotfiles."
