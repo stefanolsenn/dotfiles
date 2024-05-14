a3a10705fe1ad42855c40f6cdb78a0746910e897
Stefan <23049696+stefanolsenn@users.noreply.github.com>
2024-05-14 19:52:51 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,29 +1,28 @@
 #!/bin/bash -e
 
-function dotfiles {
-   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
-}
-
-rm -rf $HOME/.dotfiles
-git clone --bare git@github.com:stefanolsenn/dotfiles.git $HOME/.dotfiles
-
-mkdir -p $HOME/.dotfiles-backups
-dotfiles checkout -q &> /dev/null
-
-if [[ $? == 0 ]]; then
-  echo "Checked out dotfiles.";
-else
-  echo "Backing up pre-existing dot files.";
-  dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $HOME/.dotfiles-backups/{}
-  
-  if [[ $? -ne 0 ]]; then
-     echo "Something went wronng"
-  else
-     dotfiles checkout
-     dotfiles config status.showUntrackedFiles no
-     source $HOME/.bashrc
-     echo "OK!"
-  fi
+if [ -d "$HOME/.dotfiles" ]; then
+    rm -rf "$HOME/.dotfiles"
 fi
 
+git clone --bare git@github.com:stefanolsenn/dotfiles.git "$HOME/.dotfiles"
+mkdir -p "$HOME/.dotfiles-backups"
+
+if dotfiles checkout -q &>/dev/null; then
+    echo "Checked out dotfiles."
+    echo "OK!"
+else
+    echo "Backing up pre-existing dot files."
+
+    dotfiles checkout 2>&1 | grep "^\s+" | awk '{print $1}' | xargs -I{} mv {} "$HOME/.dotfiles-backups/{}"
+
+    if [ $? -ne 0 ]; then
+        echo "Failed to backup dotfiles."
+        exit 1
+    fi
+
+    dotfiles checkout
+    dotfiles config status.showUntrackedFiles no
+    source "$HOME/.bashrc"
+    echo "OK!"
+fi
 
