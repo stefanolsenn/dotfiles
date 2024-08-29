140be85a231d213dd16dfbabfdd66fc6b0245d98
Stefan Olsen <stol@cloudfactory.dk>
2024-08-29 14:28:48 +0200

    replace only if newer

--- .scripts/appsettings.sh
+++ .scripts/appsettings.sh
@@ -1,11 +1,12 @@
 source="appsettings.override.json"
 
+
 if [[ $(which op) == '' ]]; then
 	echo "You need to install the 1password-cli first"
 	exit 1
 fi
 
-value=$(op read op://Shared/alpha-appsettings.override.json/appsettings.override.json)
+value=$(op read op://Shared/alpha-appsettings.override.json/appsettings.override.json | jq)
 
 if [[ $? -ne 0 ]]; then
 	exit $?
@@ -26,6 +27,16 @@ if [[ ${#dests} -eq 0 ]]; then
 	done
 fi
 for dest in "${dests[@]}"; do
+	if [[ -f $dest/$source ]]; then
+		existing=$(jq -R 'fromjson? | .' $dest/$source)
+		is_same=$(jq -n --arg a "$existing" --arg b "$value" '$a == $b' 2> /dev/null || false)
+
+
+		if [[ $is_same == true ]]; then
+			echo "$dest is up to date"
+			continue
+		fi
+	fi
 	echo "$source -> $dest"
 	echo $value > $dest/$source
 done
