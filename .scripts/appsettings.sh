ab38285728d88d3d65d1247be5ae66287ac6da8f
Stefan Olsen <stol@cloudfactory.dk>
2024-09-04 09:16:55 +0200

    appsettings.sh

--- .scripts/appsettings.sh
+++ .scripts/appsettings.sh
@@ -6,13 +6,25 @@ if [[ $(which op) == '' ]]; then
 	exit 1
 fi
 
-value=$(op read op://Shared/alpha-appsettings.override.json/appsettings.override.json | jq)
+if [[ $(which 1password) == '' ]]; then
+	echo "You need to install the 1password desktop app first"
+	exit 1
+fi
+
+if [[ $(pgrep 1password) == '' ]]; then
+    	nohup 1password --silent >/dev/null 2>&1 &
+	disown
+	sleep 1
+fi
+
+value=$(op read op://Shared/alpha-appsettings.override.json/appsettings.override.json)
 
 if [[ $? -ne 0 ]]; then
 	exit $?
 fi
 
 dests=($1)
+value_parsed=$(echo $value | jq)
 
 if [[ ${#dests} -eq 0 ]]; then
 	search=$(grep -ril 'UseAzureKeyVault(' --include \*.cs)
@@ -29,7 +41,7 @@ fi
 for dest in "${dests[@]}"; do
 	if [[ -f $dest/$source ]]; then
 		existing=$(jq -R 'fromjson? | .' $dest/$source)
-		is_same=$(jq -n --arg a "$existing" --arg b "$value" '$a == $b' 2> /dev/null || false)
+		is_same=$(jq -n --arg a "$existing" --arg b "$value_parsed" '$a == $b' 2> /dev/null || false)
 
 
 		if [[ $is_same == true ]]; then
@@ -37,6 +49,7 @@ for dest in "${dests[@]}"; do
 			continue
 		fi
 	fi
+
 	echo "$source -> $dest"
-	echo $value > $dest/$source
+	echo $value_parsed > $dest/$source
 done
