41fffc685ef394fff56eeed02da1085e581ac630
Stefan Olsen <stol@cloudfactory.dk>
2024-10-07 10:05:43 +0200

    amtool and promtool install

---
+++ .scripts/install-amtool.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+# Fetch latest version
+TAG=$(curl -s https://api.github.com/repos/prometheus/alertmanager/releases | jq -r '.[] | .tag_name' | head -n 1)
+echo $TAG
+
+# Remove the `v` prefix
+VERSION=$(echo $TAG | sed 's/v//')
+echo $VERSION
+
+# Check CPU architecture
+ARCH=$(uname -s | tr A-Z a-z)-$(uname -m | sed 's/x86_64/amd64/')
+echo $ARCH
+
+# Download the release
+curl -L -O https://github.com/prometheus/alertmanager/releases/download/$TAG/alertmanager-$VERSION.$ARCH.tar.gz
+
+# Extract the file
+tar -xvzf alertmanager-$VERSION.$ARCH.tar.gz
+
+# Move promtool
+mv alertmanager-$VERSION.$ARCH/amtool /usr/local/bin
+
+# Verify if promtool is installed
+amtool --version
+
+# Delete downloaded files
+rm -rf alertmanager-$VERSION.$ARCH
+rm -rf alertmanager-$VERSION.$ARCH.tar.gz
---
+++ .scripts/install-promtool.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+# Fetch latest version
+TAG=$(curl -s https://api.github.com/repos/prometheus/prometheus/releases | jq -r '.[] | .tag_name' | head -n 1)
+echo $TAG
+
+# Remove the `v` prefix
+VERSION=$(echo $TAG | sed 's/v//')
+echo $VERSION
+
+# Check CPU architecture
+ARCH=$(uname -s | tr A-Z a-z)-$(uname -m | sed 's/x86_64/amd64/')
+echo $ARCH
+
+# Download the release
+curl -L -O https://github.com/prometheus/prometheus/releases/download/$TAG/prometheus-$VERSION.$ARCH.tar.gz
+
+# Extract the file
+tar -xvzf prometheus-$VERSION.$ARCH.tar.gz
+
+# Move promtool
+mv prometheus-$VERSION.$ARCH/promtool /usr/local/bin
+
+# Verify if promtool is installed
+promtool --version
+
+# Delete downloaded files
+rm -rf prometheus-$VERSION.$ARCH
+rm -rf prometheus-$VERSION.$ARCH.tar.gz
